d382ec9422a761d6355c19af4275439b3a3dc0a3
natnattt <164592034+natnattt@users.noreply.github.com>
2024-05-09 17:17:52 +0700

    etl.py

--- postgresql/etl.py
+++ postgresql/etl.py
@@ -10,12 +10,24 @@ conn_string = f'postgresql://{user}:{password}@{hostname}:{port}/{database}'
 engine      = create_engine(conn_string)
 conn        = engine.connect()
 
-# ---------- Buat Ambil dan jadiin Table di database------
-df = pd.read_csv('https://raw.githubusercontent.com/salbifaza/datasets/main/diamond/diamonds.csv')
-df = df[['carat','color','clarity']]
-df.to_sql("diamond",engine, if_exists='replace')
+# Contoh 1: Import data CSV
+df = pd.read_csv('https://raw.githubusercontent.com/natnattt/ETL-Project/main/database/final_dataset.csv')
 
-# ---------- Ngambil di database buat jadi dataframe ----------
+# Memuat data dari URL ke dalam DataFrame
+df = pd.read_csv(url)
+
+# Transform: Mengganti kolom 'BMIcase' dengan format normal
+df['BMIcase'] = df['BMIcase'].str.capitalize()
+
+# Transform: Melakukan pembulatan tanpa koma pada kolom 'Weight', 'Height', dan 'BMI' dan menambahkan 'kg' dan 'm'
+df['Weight'] = df['Weight'].round().astype(int).astype(str) + ' kg'
+df['Height'] = df['Height'].round().astype(int).astype(str) + ' m'
+df['BMI'] = df['BMI'].round(0).astype(int)
+
+# Load ke SQL
+df.to_sql("database",engine, if_exists='replace')
+
+# Contoh 2: Mengambil data dari database menjadi dataframe 
 query   = "SELECT * FROM pet"
 df_read = pd.read_sql(query,engine)
 df_read.head()
